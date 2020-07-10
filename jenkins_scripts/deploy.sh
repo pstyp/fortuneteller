605afadb06c8eae5db905147c6552fe42b3b51ea
pstyp <pstyp94@gmail.com>
2020-07-10 15:13:38 +0000

    changed deploy script

--- jenkins_scripts/deploy.sh
+++ jenkins_scripts/deploy.sh
@@ -3,15 +3,15 @@
 source /home/jenkins/env_var.sh 
 
 cd .. << EOF
-docker-compose build
-docker-compose push
+sudo docker-compose build
+sudo docker-compose push
 
 EOF
 
 ssh pstyp94@project2 << EOF
 
-docker stack deploy --compose-file docker-compose.yaml project
+sudo docker stack deploy --compose-file docker-compose.yaml project
 
-docker exec $(docker ps -q -f name=project_service1) python3 create.py
+sudo docker exec $(docker ps -q -f name=project_service1) python3 create.py
 
 EOF
