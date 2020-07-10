1cad8cc799f2e66659bc3a8167ac40c66061f2a0
pstyp <pstyp94@gmail.com>
2020-07-10 10:23:55 +0000

    edited jenkins files

--- jenkins_scripts/pytest.sh
+++ jenkins_scripts/pytest.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+source /home/jenkins/env_var.sh
+
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
 pip3 install pytest pytest-cov 
 pwd
 cd ..
 
+pip3 install -r ./service1/requirements.txt
 
 python3 -m pytest --cov ./service1/application
 
