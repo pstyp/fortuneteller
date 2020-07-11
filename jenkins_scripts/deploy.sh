63064559814027f114a14d285f9b282b76c1fe7d
pstyp <pstyp94@gmail.com>
2020-07-11 13:38:14 +0000

    changed script

--- jenkins_scripts/deploy.sh
+++ jenkins_scripts/deploy.sh
@@ -10,7 +10,9 @@ sudo -E docker-compose push
 scp docker-compose.yaml jenkins@project2:/home/jenkins/
 
 ssh jenkins@project2 << EOF
+export DATABASE=${DATABASE}
+export PASSWORD=${PASSWORD}
 docker stack deploy --compose-file docker-compose.yaml project
-sleep 5
+
 docker exec \$(docker ps -q -f name=project_service1) python3 create.py
 EOF
