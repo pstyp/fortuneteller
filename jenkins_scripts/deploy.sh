11d78818ebae340353c4f876ccf7b4c4d74c9516
pstyp <pstyp94@gmail.com>
2020-07-11 14:38:11 +0000

    changed script

--- jenkins_scripts/deploy.sh
+++ jenkins_scripts/deploy.sh
@@ -12,7 +12,7 @@ scp docker-compose.yaml jenkins@project2:/home/jenkins/
 ssh jenkins@project2 << EOF
 export DATABASE=${DATABASE}
 export PASSWORD=${PASSWORD}
-sudo docker stack deploy --compose-file docker-compose.yaml project
+sudo -E docker stack deploy --compose-file docker-compose.yaml project
 
-sudo docker exec \$(docker ps -q -f name=project_service1 | head -n 1) python3 create.py
+sudo -E docker exec \$(docker ps -q -f name=project_service1 | head -n 1) python3 create.py
 EOF
